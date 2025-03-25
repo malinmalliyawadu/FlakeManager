590ed4f2e63e54eca240f4f15b2cdcd1ddf15bf9
Malin Malliya Wadu <malin.malliya.wadu@gmail.com>
2025-03-25 17:20:27 +1300

    Fix: Configure Netlify for proper SSR deployment

--- remix.config.js
+++ remix.config.js
@@ -6,9 +6,10 @@ export default {
   serverBuildPath: "build/index.js",
   assetsBuildDirectory: "public/build",
   future: {
-    v2_errorBoundary: true,
-    v2_meta: true,
-    v2_normalizeFormMethod: true,
-    v2_routeConvention: true,
+    v3_fetcherPersist: true,
+    v3_lazyRouteDiscovery: true,
+    v3_relativeSplatPath: true,
+    v3_singleFetch: true,
+    v3_throwAbortReason: true,
   },
 };
