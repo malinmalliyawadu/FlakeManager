c180c97e2c249b9f858c1470f0da77011002860e
Malin Malliya Wadu <malin.malliya.wadu@gmail.com>
2025-03-25 17:17:14 +1300

    Update netlify.toml to set force redirection to false and remove unused stylesheet link from index.html for cleaner deployment configuration.

--- remix.config.js
+++ remix.config.js
@@ -5,4 +5,10 @@ export default {
   // Standard Remix configuration for Netlify
   serverBuildPath: "build/index.js",
   assetsBuildDirectory: "public/build",
+  future: {
+    v2_errorBoundary: true,
+    v2_meta: true,
+    v2_normalizeFormMethod: true,
+    v2_routeConvention: true,
+  },
 };
