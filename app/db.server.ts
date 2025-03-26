4c7ebc391712ea6eb88661304d6c00b68949258e
Malin Malliya Wadu <malin.malliya.wadu@gmail.com>
2025-03-26 15:24:37 +1300

    Enhance database connection handling in Prisma and update Netlify configuration
    - Updated netlify.toml to include environment variable placeholders for DATABASE_URL and MIGRATION_DATABASE_URL.
    - Refactored db.server.ts to improve database connection error handling and logging based on the environment.

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,9 +1,22 @@
 import { PrismaClient } from "@prisma/client";
 
-import { singleton } from "./singleton.server";
+import { singleton } from "~/singleton.server";
 
 // Hard-code a unique key, so we can look up the client when this module gets re-imported
-const prisma = singleton("prisma", () => new PrismaClient());
-prisma.$connect();
+const prisma = singleton("prisma", () => {
+  const client = new PrismaClient({
+    log:
+      process.env.NODE_ENV === "development"
+        ? ["query", "error", "warn"]
+        : ["error"],
+  });
+
+  // Connect to the database
+  client.$connect().catch((error) => {
+    console.error("Failed to connect to the database:", error);
+  });
+
+  return client;
+});
 
 export { prisma };
